b6811e7e45fcb49a3396837031775bea3b6a620b
Mightyprincess <happyrabbit62@gmail.com>
2023-02-20 23:54:47 +0300

    feat: solve brackets

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  // create bracketsMap
+  const bracketsMap = {};
+  for (let key of bracketsConfig) {
+    bracketsMap[key[0]] = key[1];
+  }
+
+  const stack = [];
+  for (let elem of str) {
+    if (elem in bracketsMap) {
+      // opening bracket
+      if (stack[stack.length - 1] === elem) {
+        stack.pop();
+      } else {
+        stack.push(bracketsMap[elem])
+      }
+    } else if (stack[stack.length - 1] === elem) {
+      // closing bracket pair
+      stack.pop();
+    } else {
+      // closing bracket without pair
+      return false;
+    }
+  }
+
+  return !stack.length;
 }
